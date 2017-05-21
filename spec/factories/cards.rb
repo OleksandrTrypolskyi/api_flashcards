35c04adf998520e2779dea18fb69d39457219604
TrypolskyiS <tripolskys@gmail.com>
2017-05-21 22:48:53 +0300

    Fix Travis

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -2,10 +2,5 @@ FactoryGirl.define do
   factory :card, class: ApiFlashcards::Card do
     original_text 'дом'
     translated_text 'house'
-    interval 1
-    repeat 1
-    efactor 2.5
-    quality 5
-    attempt 1
   end
 end
